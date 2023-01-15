5c6404c59438ab841872621b2f1fd42117e7c367
Kratika Tugnawat <kratikatugnawat7@gmail.com>
2023-01-16 00:26:07 +0530

    check

---
+++ lib/consts/strings.dart
@@ -0,0 +1,6 @@
+const String degree = "°";
+const String apiKey = "8ba21b74f8a04f5436afb406bd439e49";
+const String currentEndpoint = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+const String daysEndpoint = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+const String latitude = "31.582045";
+const String longitude = "74.329376";
